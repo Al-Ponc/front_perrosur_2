73d4b794c59b34904e02b5e90990bf2b35e6c977
Al-Ponc <tengocada@gmail.com>
2023-09-30 10:59:04 -0300

    ...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const serverDomain = process.env.SERVER_DOMAIN;
+
 const nextConfig = {
   i18n: {
     locales: ['es'],
@@ -12,16 +12,6 @@ const nextConfig = {
       'pk.eyJ1IjoibWF4bmF2ZWlsbGFuIiwiYSI6ImNsMGlpcGg3ODAyejgzcGtjcXE3NW5lMTkifQ.zBrBdAmdc22oepPkr1cdqA',
     BASE_URL: process.env.URL,
     SERVER_URL: process.env.SERVER_URL,
-  },
-  images: {
-    domains: [
-      serverDomain ? serverDomain : undefined,
-      'lh3.googleusercontent.com',
-      'localhost',
-      'tallerperrosur.cl',
-    ],
-    disableStaticImages: true,
-    deviceSizes: [640, 768, 1024, 1280, 1536],
   },
 };
 
